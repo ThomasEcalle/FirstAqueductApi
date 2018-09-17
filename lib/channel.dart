abd3c26b1e17cf500bb05bea7adbc91d4e9deef0
thomasecalle <thomasecalle@hotmail.fr>
2018-09-18 01:56:36 +0200

    First commit

---
+++ lib/channel.dart
@@ -0,0 +1,42 @@
+import 'heroes.dart';
+import 'controller/HeroesController.dart';
+
+class HeroesChannel extends ApplicationChannel {
+  ManagedContext context;
+
+  @override
+  Future prepare() async {
+    logger.onRecord.listen(
+        (rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
+
+    final config = HeroConfig(options.configurationFilePath);
+    final dataModel = ManagedDataModel.fromCurrentMirrorSystem();
+    final persistentStore = PostgreSQLPersistentStore.fromConnectionInfo(
+        config.database.username,
+        config.database.password,
+        config.database.host,
+        config.database.port,
+        config.database.databaseName);
+
+    context = ManagedContext(dataModel, persistentStore);
+  }
+
+  @override
+  Controller get entryPoint {
+    final router = Router();
+
+    router.route('/heroes/[:id]').link(() => HeroesController(context));
+
+    router.route("/example").linkFunction((request) async {
+      return Response.ok({"key": "value"});
+    });
+
+    return router;
+  }
+}
+
+class HeroConfig extends Configuration {
+  HeroConfig(String path) : super.fromFile(File(path));
+
+  DatabaseConfiguration database;
+}
